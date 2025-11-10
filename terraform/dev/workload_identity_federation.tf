a228b652fb92f14082c0bdbaa1c7faf5bc5a5ce9
serhii <serhii.puzyrov@gmail.com>
2025-11-10 15:36:41 +0100

    Update

--- terraform/dev/workload_identity_federation.tf
+++ terraform/dev/workload_identity_federation.tf
@@ -10,6 +10,8 @@ locals {
     "roles/resourcemanager.projectIamAdmin",
     "roles/iam.serviceAccountAdmin",
     "roles/iam.serviceAccountKeyAdmin",
+    "roles/iam.workloadIdentityPoolAdmin",
+    "roles/iam.serviceAccountTokenCreator"
   ]
 }
 resource "google_project_iam_member" "terraform_sa_roles" {
