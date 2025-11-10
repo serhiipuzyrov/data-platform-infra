d5337d4259bbae0f02310fd6466f2f91329ca308
serhii <serhii.puzyrov@gmail.com>
2025-11-10 16:46:48 +0100

    Update

--- terraform/prod/workload_identity_federation.tf
+++ terraform/prod/workload_identity_federation.tf
@@ -10,8 +10,17 @@ locals {
     "roles/resourcemanager.projectIamAdmin",
     "roles/iam.serviceAccountAdmin",
     "roles/iam.serviceAccountKeyAdmin",
+    "roles/iam.workloadIdentityPoolAdmin",
+    "roles/iam.serviceAccountTokenCreator",
+    "roles/iam.serviceAccountUser",
+    "roles/storage.admin"
+  ]
+  allowed_repositories = [
+    "${var.github_org}/${var.github_repo_infra}",
+    "${var.github_org}/${var.github_repo_dbt}"
   ]
 }
+
 resource "google_project_iam_member" "terraform_sa_roles" {
   for_each = toset(local.terraform_sa_roles)
   project = var.project_id
@@ -21,13 +30,14 @@ resource "google_project_iam_member" "terraform_sa_roles" {
 
 # Workload Identity Pool
 resource "google_iam_workload_identity_pool" "github_pool" {
+  project = var.project_id
   workload_identity_pool_id = "github-actions-pool"
   display_name              = "GitHub Actions Pool"
   description               = "OIDC pool for GitHub Actions"
 }
 
-# Workload Identity Provider (GitHub)
 resource "google_iam_workload_identity_pool_provider" "github_provider" {
+  project = var.project_id
   workload_identity_pool_id          = google_iam_workload_identity_pool.github_pool.workload_identity_pool_id
   workload_identity_pool_provider_id = "github-provider"
   display_name                       = "GitHub Provider"
@@ -35,21 +45,20 @@ resource "google_iam_workload_identity_pool_provider" "github_provider" {
   oidc {
     issuer_uri = "https://token.actions.githubusercontent.com"
   }
-  # Only map attributes; do NOT set attribute_condition here
   attribute_mapping = {
     "google.subject"       = "assertion.sub"
     "attribute.actor"      = "assertion.actor"
     "attribute.repository" = "assertion.repository"
     "attribute.ref"        = "assertion.ref"
   }
-  # Add attribute condition to restrict to your repository
-  attribute_condition = "assertion.repository == '${var.github_org}/${var.github_repo}'"
+  # Update attribute condition to allow multiple repositories
+  attribute_condition = "assertion.repository in ['${join("', '", local.allowed_repositories)}']"
 }
 
 # Allow only your repo to impersonate the terraform-sa
 resource "google_service_account_iam_member" "github_wif_binding" {
+  for_each = toset(local.allowed_repositories)
   service_account_id = google_service_account.terraform_sa.name
   role               = "roles/iam.workloadIdentityUser"
-  # The attribute below must match the mapping defined above
-  member = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.github_pool.name}/attribute.repository/${var.github_org}/${var.github_repo}"
+  member = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.github_pool.name}/attribute.repository/${each.value}"
 }
