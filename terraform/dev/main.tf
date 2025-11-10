0f13139c95f07aceb0d02169e91779be44cbb67f
serhii <serhii.puzyrov@gmail.com>
2025-11-10 11:17:29 +0100

    Update

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -9,9 +9,8 @@ terraform {
 }
 
 provider "google" {
-  project               = var.project_id
-  user_project_override = true
-  billing_project       = var.project_id
+  project = var.project_id
+  region  = var.region
 }
 
 terraform {
